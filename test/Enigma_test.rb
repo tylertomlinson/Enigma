57bfa2ff81e06a450eab04a52d3cf5af48058937
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-27 19:08:16 -0700

    Tests created for each way encrypt can have arguments passed.

--- test/Enigma_test.rb
+++ test/Enigma_test.rb
@@ -19,36 +19,44 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Hash, @engima.encrypt
     assert_instance_of Hash, @enigma.decrypt
   end
+
+  def test_can_encrypt_message_with_all_arguments_being_passed
+    expected = {
+                  encryption: "keder ohulw",
+                  key: "02715",
+                  date: "040895"
+                }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_can_encrypt_message_with_only_message_and_key_being_passed
+    expected = {
+                  encryption: "keder ohulw",
+                  key: "02715",
+                  date: "040895"
+                }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715")
+  end
+
+  def test_can_encrypt_message_with_only_message_and_date_being_passed
+    expected = {
+                  encryption: "keder ohulw",
+                  key: "02715",
+                  date: "040895"
+                }
+
+    assert_equal expected, @enigma.encrypt("hello world", "040895")
+  end
+
+  def test_can_encrypt_message_with_only_message_being_passed
+    expected = {
+                  encryption: "keder ohulw",
+                  key: "02715",
+                  date: "040895"
+                }
+
+    assert_equal expected, @enigma.encrypt("hello world")
+  end
 end
-
-
-
-
-# pry(main)> enigma.encrypt("hello world", "02715", "040895")
-#
-#   {
-#     encryption: "keder ohulw",
-#     key: "02715",
-#     date: "040895"
-#   }
-
-# decrypt a message with a key and date
-# pry(main) > enigma.decrypt("keder ohulw", "02715", "040895")
-# #=>
-# #   {
-# #     decryption: "hello world",
-# #     key: "02715",
-# #     date: "040895"
-# #   }
-#
-# # encrypt a message with a key (uses today's date)
-# pry(main)> encrypted = enigma.encrypt("hello world", "02715")
-# #=> # encryption hash here
-#
-# #decrypt a message with a key (uses today's date)
-# pry(main) > enigma.decrypt(encrypted[:encryption], "02715")
-# #=> # decryption hash here
-#
-# # encrypt a message (generates random key and uses today's date)
-# pry(main)> enigma.encrypt("hello world")
-# #=> # encryption hash here
