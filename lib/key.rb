d9be47205c0076f5a5a8932607d9890fd665d9d8
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-29 19:48:16 -0700

    Add keys as argument Moved test for random numbers to key_test Changed key_generator to be number_generator

--- lib/key.rb
+++ lib/key.rb
@@ -1,2 +1,12 @@
+require_relative 'generator'
+
 class Key
+  include Generator
+  extend Generator
+  
+  attr_reader :key
+
+  def initialize(key)
+    @key = key
+  end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -5,11 +5,16 @@ require './lib/key'
 class KeyTest < Minitest::Test
 
   def setup
-    @key = Key.new
+    @key = Key.new("02715")
   end
 
   def test_it_exists
     assert_instance_of Key, @key
   end
 
+  def test_can_generate_5_random_numbers
+    @key.stubs(:number_generator).returns([4,5,2,3,6])
+
+    assert_equal [4,5,2,3,6], @enigma.random_keys
+  end
 end
