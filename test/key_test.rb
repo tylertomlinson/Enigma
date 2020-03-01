59214ddd1e73e4aeecd1da8277ee507f14d6f82f
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-29 19:36:53 -0700

    Create key class with test Testing for instance of key

---
+++ test/key_test.rb
@@ -0,0 +1,15 @@
+require_relative 'test_helper'
+require './lib/generator'
+require './lib/key'
+
+class KeyTest < Minitest::Test
+
+  def setup
+    @key = Key.new
+  end
+
+  def test_it_exists
+    assert_instance_of Key, @key
+  end
+
+end
