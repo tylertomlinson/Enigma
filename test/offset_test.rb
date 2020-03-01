07ff6919ea3e60c67cbc30afd0dc032387194622
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-01 14:50:19 -0700

    testing offset attribute (see more) Added pseudo for what needs to be tested. WIP on figuring out user date and default date being squared the same way.

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -5,7 +5,7 @@ require './lib/offset'
 class OffsetTest < Minitest::Test
 
   def setup
-    @offset = Offset.new
+    @offset = Offset.new("040895")
   end
 
   def test_it_exists
@@ -13,13 +13,25 @@ class OffsetTest < Minitest::Test
   end
 
   def test_attributes
-    skip
+    assert_equal "040895", @offset.date
   end
 
-  def test_can_format_date_for_offset
-    Date.stubs(:today).returns(Date.new(2020,2,27))
+  #need to test
+  #if it has date (from user)
+  #can get default_date
+  #can convert users date
+  #can convert default_date
+  #build hash like keys
 
-    assert_equal ["1", "5", "2", "9"], @offset.convert_date
+
+  def test_can_format_generated_date_for_offset
+    #this needs to be testing for user input as well
+    offset1 = Offset.new()
+  Date.stubs(:today).returns(Date.new(2020,2,27))
+
+    offset1.stubs(:default_date).returns(2020227)
+
+    assert_equal ["1", "5", "2", "9"], offset1.convert_date
   end
 
   def test_can_generate_current_date
