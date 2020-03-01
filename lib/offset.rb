6606cf137a916ff3ebd3c7986c440eb7fc9d01a4
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-01 14:50:31 -0700

    WIP on last commit

--- lib/offset.rb
+++ lib/offset.rb
@@ -4,8 +4,16 @@ class Offset
   include Generator
   extend Generator
 
+  attr_reader :date
+
+  def initialize(date = default_date)
+    @date = date
+  end
+
   def convert_date
-    squared_date = default_date
+    squared_date = @date.to_i
     (squared_date *= squared_date).to_s[-4, 4].chars
   end
+
+
 end
