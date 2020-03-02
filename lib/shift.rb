a347af87e26b854d13845b49755e408b65e2f8c2
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-01 19:27:11 -0700

    Finally got it working Passes test. Rake passing all tests, which aren't skipped SimpleCov at 100%

--- lib/shift.rb
+++ lib/shift.rb
@@ -4,4 +4,8 @@ class Shift
     @key = key
     @offset = offset
   end
+
+  def shifter
+    @key.key_splitter.merge(@offset.offset_hash) { |key, key_value, offset_value| key_value + offset_value }
+  end
 end
