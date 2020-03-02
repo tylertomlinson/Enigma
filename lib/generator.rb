8093960f5794bd13c29cb0a4bdf96f18d8403a6e
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-02 01:30:47 -0700

    Character set lives in generator

--- lib/generator.rb
+++ lib/generator.rb
@@ -2,6 +2,10 @@ require 'date'
 
 module Generator
 
+  def character_set
+    ("a".."z").to_a << " "
+  end
+
   def number_generator
     rand(0..99999).to_s.rjust(5, "0")
   end
@@ -9,5 +13,4 @@ module Generator
   def default_date
     Date.today.strftime("%y%m%d").to_i
   end
-
 end
