cf32586b2ca315354e9fdb8c2040d604866c42ac
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-02 22:47:17 -0700

    Test created for encrypt and decrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,11 +4,6 @@ class EnigmaTest < Minitest::Test
 
   def setup
     @enigma = Enigma.new
-    @expected = {
-              encryption: "keder ohulw",
-              key: "02715",
-              date: "040895"
-            }
   end
 
   def test_it_exists
@@ -16,15 +11,28 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_encrypt_and_decrypt_returns_as_hash
-    skip
-    assert_instance_of Hash, @engima.encrypt
-    assert_instance_of Hash, @enigma.decrypt
+
+    assert_instance_of Hash, @enigma.encrypt("hello world", "02715", "040895")
+    assert_instance_of Hash, @enigma.decrypt("keder ohulw", "02715", "040895")
   end
 
-  def test_can_encrypt_message_with_all_arguments_being_passed
-    skip
-    assert_equal @expected, @enigma.encrypt("hello world", "02715", "040895")
+  def test_can_encrypt
+    expected = {
+          encryption: "keder ohulw",
+          key: "02715",
+          date: "040895"
+        }
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
   end
+
+  def test_can_decrypt
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+ end
 
   def test_can_encrypt_message_with_only_message_and_key_being_passed
     skip
